ee5bdf81f6e081b9fc2b9f8da87d698ee1a84a77
Logadheep <logadheep3438@gmail.com>
2022-11-25 16:24:53 +0530

    Changes to File structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import InputContainer from './InputContainer/InputContainer';
-import Input from "./Input/Input";
+// import InputContainer from './components/InputContainer/InputContainer';
+import Input from "./components/Input/Input";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
